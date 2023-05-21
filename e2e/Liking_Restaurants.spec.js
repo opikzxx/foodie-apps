1aeaa6825a1bcc0c8a5c702735e1b2bd7af9dce9
opikzxx <taufikheryunanto@gmail.com>
2023-05-21 18:23:27 +0700

    update

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,5 +1,34 @@
-Feature('Liking Movies');
- 
-Scenario('test something', ({ I }) => {
- 
+Feature('Liking Restaurants');
+
+Before(({ I }) => {
+  I.amOnPage('/#/like');
+});
+
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.see(
+    'Tidak ada restaurant untuk ditampilkan',
+    '.restaurant-item__not__found',
+  );
+});
+
+Scenario('like one restaurant', ({ I }) => {
+  I.see(
+    'Tidak ada restaurant untuk ditampilkan',
+    '.restaurant-item__not__found',
+  );
+
+  I.amOnPage('/');
+  I.wait(1);
+  I.scrollTo('.product-card');
+
+  I.seeElement('.product-card');
+  I.wait(1);
+  I.click(locate('.product-card').first());
+  I.wait(1);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.product-card');
 });
